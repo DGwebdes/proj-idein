95d1bdc835857a2c0a5dee45f2849d9393d5ffaa
DGwebdes <alan.bilik8@gmail.com>
2025-03-31 21:43:21 +0100

    better names for env variables

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -15,7 +15,7 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
+    apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
