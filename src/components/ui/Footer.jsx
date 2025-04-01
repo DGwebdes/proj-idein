93d611dd9a0181eb1d0fc3ac5bbc7668e41b789b
DGwebdes <alan.bilik8@gmail.com>
2025-04-01 03:06:36 +0100

    updated footer icons and favicon image

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div className="w-full py-6 px-8 bg-gradient-to-r from-blue-600 to-purple-700 text-white flex justify-between items-center shadow-md">
             <div className="text-sm">
-                <p>&copy; 2025 Idein. All Rights Reserved.</p>
+                <p>
+                    &copy; 2025 Idein. All Rights Reserved. Developed by{" "}
+                    <a href="https://dielanwebdev.pt/" target="_blank">
+                        <strong>Dielan Garve</strong>
+                    </a>
+                </p>
             </div>
             <div className="flex gap-4">
                 <a
@@ -12,21 +20,21 @@ const Footer = () => {
                     target="_blank"
                     className="text-white hover:text-gray-300 transition-colors duration-200"
                 >
-                    GitHub
+                    <FaGithub />
                 </a>
                 <a
-                    href="https://www.linkedin.com"
+                    href="https://www.linkedin.com/in/alangarve/"
                     target="_blank"
                     className="text-white hover:text-gray-300 transition-colors duration-200"
                 >
-                    LinkedIn
+                    <FaLinkedin />
                 </a>
                 <a
-                    href="https://twitter.com"
+                    href="https://x.com/gandalfsneto"
                     target="_blank"
                     className="text-white hover:text-gray-300 transition-colors duration-200"
                 >
-                    Twitter
+                    <FaTwitter />
                 </a>
             </div>
         </div>
