230595c2576b67c624395a77d17af29cc01dafa0
DGwebdes <alan.bilik8@gmail.com>
2025-04-03 05:06:58 +0100

    forgot to change dev keys to prod keys

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ClerkProvider publishableKey={DEV_KEY} frontendAPI={DEV_URL}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} frontendAPI={API_URL}>
             <App />
         </ClerkProvider>
     </StrictMode>,
